15759bd4234873f94bb6e319d6c97eaf72408e25
jugerh <juliangerhards2@gmail.com>
2021-10-07 04:41:54 +0200

    Testing

--- CleanUIDemo/ContentView.swift
+++ CleanUIDemo/ContentView.swift
@@ -11,26 +11,31 @@ import CleanUI
 struct ContentView: View {
     @State private var text: String = ""
     var body: some View {
-        VStack(spacing: 0) {
+        NavigationView {
             List {
-                Text(35521.abbreviate())
-                Text(554.abbreviate())
-                Text(423546.abbreviate())
-                Text(26546546.abbreviate())
-                Text(9453.abbreviate())
-                Text("Test".md5)
-                Text("Hallo&yeay".urlEncode())
-                Text("Hallo=yeay".urlEncode())
-                Text("Hallo/yeay".urlEncode())
-                Text("Hallo?yeay".urlEncode())
+                
+                Section(header: FeedTitle("TextEditors")) {
+                    NavigationLink(destination: {
+                        VStack(spacing: 20) {
+                            Text("SingleLineTextEditor() is a single line text editor which expands on line break. Besides that, SingleLineTextEditor() is able to show hashtags, mentions and links in realtime.")
+                            SingleLineTextEditor($text)
+                        }
+                        .padding()
+                        .navigationBar("SingleLineTextEditor()")
+                    }) {
+                        Text("SingleLineTextEditor()")
+                    }
+                    
+                    NavigationLink(destination: {
+                        BetterTextEditor($text)
+                            .padding()
+                            .navigationBar("BetterTextEditor()")
+                    }) {
+                        Text("BetterTextEditor()")
+                    }
+                }
             }
+            .navigationBar("CleanUI Demo", bigTitle: true)
         }
-        .navigationBar("Test", bigTitle: true, searchBar: NavigationBarSearchField())
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
