d23dc0414c4ba59a21a2355c24c8cba21dcea40c
jugerh <96697201+juliangerhards@users.noreply.github.com>
2022-03-14 14:39:37 +0100

    Added full-width back gesture again without the pop and push transition (UINavigationBar delegate)

---
+++ Sources/CleanUI/Extensions/UIKit/UINavigationController.swift
@@ -0,0 +1,65 @@
+//  Copyright © 2021 - present Julian Gerhards
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//  GitHub https://github.com/knoggl/iOSFullWidthBackGesture
+//
+
+import SwiftUI
+import Combine
+
+/// This is extension is mainly to provide a fullscreen back gesture.
+extension UINavigationController: UIViewControllerTransitioningDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        interactivePopGestureRecognizer?.delegate = self
+        setupFullWidthBackGesture()
+    }
+    
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        // Removes the back button text in NaviagtionBar
+        navigationBar.topItem?.backButtonDisplayMode = .minimal
+    }
+    
+    private func setupFullWidthBackGesture() {
+        let fullWidthBackGestureRecognizer = UIPanGestureRecognizer()
+        guard
+            let interactivePopGestureRecognizer = interactivePopGestureRecognizer,
+            let targets = interactivePopGestureRecognizer.value(forKey: "targets")
+        else {
+            return
+        }
+        
+        fullWidthBackGestureRecognizer.setValue(targets, forKey: "targets")
+        fullWidthBackGestureRecognizer.delegate = self
+        view.addGestureRecognizer(fullWidthBackGestureRecognizer)
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let isSystemSwipeToBackEnabled = interactivePopGestureRecognizer?.isEnabled == true
+        let isThereStackedViewControllers = viewControllers.count > 1
+        
+        // Allow swipe only right
+        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+            let translation = panGestureRecognizer.translation(in: view)
+            if translation.x <=  0 {
+                return false
+            }
+        }
+        return isSystemSwipeToBackEnabled && isThereStackedViewControllers
+    }
+}
