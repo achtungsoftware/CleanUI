a311fb4f8c641de5f2d94aa7a491e0f99777f47c
jugerh <96697201+juliangerhards@users.noreply.github.com>
2022-04-06 19:17:17 +0200

    CLUrlImage cleaning

--- Sources/CleanUI/Views/CLUrlImage.swift
+++ Sources/CleanUI/Views/CLUrlImage.swift
@@ -22,7 +22,7 @@ import SwiftPlus
 /// Returns an ``CLUrlImage`` which fetches a image from an url, displays and caches it, if needed
 public struct CLUrlImage: View {
     
-    @ObservedObject var urlImageModel: UrlImageModel
+    @ObservedObject var model: ViewModel
     var fallbackImage: UIImage?
     var contentMode: ContentMode
     
@@ -30,93 +30,92 @@ public struct CLUrlImage: View {
     ///   - urlString: The url string to the image
     ///   - fallbackImage: The fallback image, in case the url image could not be fetched
     ///   - aspectRatio: The `contentMode`, default is `.fill`
-    public init(urlString: String?, fallbackImage: UIImage?, contentMode: ContentMode = .fill) {
-        urlImageModel = UrlImageModel(urlString: urlString)
+    public init(urlString: String, fallbackImage: UIImage?, contentMode: ContentMode = .fill) {
+        model = ViewModel(urlString: urlString)
         self.contentMode = contentMode
         self.fallbackImage = fallbackImage ?? UIColor.accent.imageWithColor(width: 1, height: 1)
     }
     
     public var body: some View {
-        Image(uiImage: ((urlImageModel.image ?? fallbackImage) ?? fallbackImage)!)
+        Image(uiImage: ((model.image ?? fallbackImage) ?? fallbackImage)!)
             .resizable()
             .aspectRatio(contentMode: contentMode)
     }
 }
 
-public class UrlImageModel: ObservableObject {
-    
-    @Published public var image: UIImage?
-    var urlString: String? = ""
-    var imageCache = ImageCache.getImageCache()
-    
-    public init(urlString: String?) {
-        self.urlString = urlString
-        loadImage()
+public extension CLUrlImage {
+    class ViewModel: ObservableObject {
+        
+        @Published public var image: UIImage?
+        var urlString: String = ""
+        var imageCache = Cache.getImageCache()
+        
+        public init(urlString: String) {
+            self.urlString = urlString
+            loadImage()
+        }
+        
+        public func loadImage() {
+            if self.loadImageFromCache() {
+                return
+            }
+            self.loadImageFromUrl()
+        }
+        
+        public func loadImageFromCache() -> Bool {
+            guard let cacheImage = imageCache.get(forKey: urlString) else {
+                return false
+            }
+            
+            self.image = cacheImage
+            
+            return true
+        }
+        
+        public func loadImageFromUrl() {
+            if let url = URL(string: urlString) {
+                let task = URLSession.shared.dataTask(with: url, completionHandler: getImageFromResponse(data:response:error:))
+                task.resume()
+            }
+        }
+        
+        
+        public func getImageFromResponse(data: Data?, response: URLResponse?, error: Error?) {
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            guard let loadedImage = UIImage(data: data) else {
+                return
+            }
+            
+            imageCache.set(forKey: urlString, image: loadedImage)
+            
+            SPThreadHelper.async.main.run {
+                self.image = loadedImage
+            }
+        }
     }
     
-    public func loadImage() {
-        if self.loadImageFromCache() {
-            return
-        }
-        self.loadImageFromUrl()
-    }
-    
-    public func loadImageFromCache() -> Bool {
-        guard let urlString = urlString else {
-            return false
+    class Cache {
+        var cache = NSCache<NSString, UIImage>()
+        
+        func get(forKey: String) -> UIImage? {
+            return cache.object(forKey: NSString(string: forKey))
         }
         
-        guard let cacheImage = imageCache.get(forKey: urlString) else {
-            return false
+        func set(forKey: String, image: UIImage) {
+            cache.setObject(image, forKey: NSString(string: forKey))
         }
         
-        self.image = cacheImage
+        private static var imageCache = Cache()
         
-        return true
-    }
-    
-    public func loadImageFromUrl() {
-        guard let urlString = urlString else {
-            return
+        static func getImageCache() -> Cache {
+            return imageCache
         }
-        
-        if(!urlString.isEmpty){
-            let url = URL(string: urlString)!
-            let task = URLSession.shared.dataTask(with: url, completionHandler: self.getImageFromResponse(data:response:error:))
-            task.resume()
-        }
-    }
-    
-    
-    public func getImageFromResponse(data: Data?, response: URLResponse?, error: Error?) {
-        guard error == nil else {
-            return
-        }
-        
-        guard let data = data else {
-            return
-        }
-        
-        guard let loadedImage = UIImage(data: data) else {
-            return
-        }
-        
-        self.imageCache.set(forKey: self.urlString!, image: loadedImage)
-        
-        SPThreadHelper.async.main.run {
-            self.image = loadedImage
-        }
-    }
-}
-
-class ImageCache {
-    var cache = NSCache<NSString, UIImage>()
-    
-    func get(forKey: String) -> UIImage? {
-        return cache.object(forKey: NSString(string: forKey))
-    }
-    
-    func set(forKey: String, image: UIImage) {
-        cache.setObject(image, forKey: NSString(string: forKey))
     }
 }
